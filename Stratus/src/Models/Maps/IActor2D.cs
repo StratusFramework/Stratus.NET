f08ee44a98d4f7b0704896823d88795df3482ffc
Christian Sagel <ckpsm@live.com>
2023-04-03 23:36:46 +0200

    Move the 2d actor actions into the models

--- Stratus/src/Models/Maps/IActor2D.cs
+++ Stratus/src/Models/Maps/IActor2D.cs
@@ -1,5 +1,4 @@
 ﻿using Stratus.Events;
-using Stratus.Models;
 using Stratus.Numerics;
 
 using System;
@@ -32,7 +31,96 @@ namespace Stratus.Models.Maps
 		event Action<Vector2Int> onMoved;
 	}
 
+	
+
+	public abstract class Actor2DCommand : Command
+	{
+		public IActor2D actor { get; }
+
+		public Actor2DCommand(IActor2D actor)
+		{
+			this.actor = actor;
+		}
+
+		public TActor As<TActor>() where TActor : IActor2D => (TActor)actor;
+
+	}
+
 	public class MovementRangeEvent : RequestResponseEvent<IActor2D, GridRange>
 	{
 	}
+}
+
+namespace Stratus.Models.Maps.Actions
+{
+	public abstract class ActorAction
+	{
+		public IActor2D actor { get; }
+		public virtual string name => GetType().Name.Replace(nameof(ActorAction), string.Empty);
+
+		public ActorAction(IActor2D actor)
+		{
+			this.actor = actor;
+		}
+
+		public TActor As<TActor>() where TActor : IActor2D => (TActor)actor;
+	}
+
+	public abstract class ActionEvent<TAction> : Event
+		where TAction : ActorAction
+	{
+		public TAction action { get; }
+
+		protected ActionEvent(TAction action)
+		{
+			this.action = action;
+		}
+
+		public override string ToString()
+		{
+			return action.ToString();
+		}
+	}
+
+	public class PreviewActionEvent : ActionEvent<ActorAction>
+	{
+		public PreviewActionEvent(ActorAction action) : base(action)
+		{
+		}
+	}
+
+	public class CancelPreviewActionEvent : ActionEvent<ActorAction>
+	{
+		public CancelPreviewActionEvent(ActorAction action) : base(action)
+		{
+		}
+	}
+
+	public class MoveActorAction : ActorAction
+	{
+		public Vector2Int sourcePosition;
+		public Vector2Int targetPosition;
+
+		public MoveActorAction(IActor2D actor) : base(actor)
+		{
+		}
+	}
+
+	public class MoveActorEvent : ActionEvent<MoveActorAction>
+	{
+		public MoveActorEvent(MoveActorAction action) : base(action)
+		{
+		}
+	}
+
+	public class WaitActorAction : ActorAction
+	{
+		public WaitActorAction(IActor2D actor) : base(actor)
+		{
+		}
+	}
+
+
+
+
 }
