fba8be6a4f52a6d77967e204409038ff1cd800dc
Christian Sagel <ckpsm@live.com>
2024-05-11 23:01:07 +0200

    Add input action prompt data structures

---
+++ Stratus/src/Input/InputActionPrompt.cs
@@ -0,0 +1,12 @@
+﻿using Stratus.Events;
+
+namespace Stratus.Inputs
+{
+	public record InputActionPrompt(string action, string message)
+	{
+	}
+
+	public record ToggleInputPromptEvent(InputActionPrompt prompt, bool toggle) : Event
+	{
+	}
+}
